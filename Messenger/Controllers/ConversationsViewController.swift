370ffa33f949939c98a586940898d24e769d7b80
Deer5000 <khidrbrinkley32@gmail.com>
2021-01-07 16:34:20 -0500

    updated login and register ui

---
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Khidr Brinkley on 12/27/20.
+//
+
+import UIKit
+
+class ConversationsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLoggedIn = UserDefaults.standard.bool(forKey: "Logged_in")
+        if !isLoggedIn {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen
+            present(nav, animated: false)
+            
+        }
+    }
+    
+}
+
