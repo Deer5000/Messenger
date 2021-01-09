34b9adee674c7422fe55f124dc30e9f62c80f81b
Deer5000 <khidrbrinkley32@gmail.com>
2021-01-09 16:10:04 -0500

    Handle auth session, add database

--- Messenger/Controllers/ConversationsViewController.swift
+++ Messenger/Controllers/ConversationsViewController.swift
@@ -6,24 +6,30 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+        
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let isLoggedIn = UserDefaults.standard.bool(forKey: "Logged_in")
-        if !isLoggedIn {
+        validateAuth()
+    }
+    
+    
+    
+    private func validateAuth() {
+        if FirebaseAuth.Auth.auth().currentUser == nil {
             let vc = LoginViewController()
             let nav = UINavigationController(rootViewController: vc)
             nav.modalPresentationStyle = .fullScreen
             present(nav, animated: false)
-            
         }
     }
     
--- Messenger/Controllers/Login/LoginViewController.swift
+++ Messenger/Controllers/Login/LoginViewController.swift
@@ -122,7 +122,10 @@ class LoginViewController: UIViewController {
         }
         
         // Firebase Log In
-        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {authResult, error in
+        FirebaseAuth.Auth.auth().signIn(withEmail: email, password: password, completion: {[weak self] authResult, error in
+            guard let strongSelf = self else {
+                return
+            }
             guard let result = authResult, error == nil else {
                 print("Failed to login user with email: \(email)")
                 return
@@ -130,7 +133,7 @@ class LoginViewController: UIViewController {
             
             let user = result.user
             print("Logged In User: \(user)")
-            
+            strongSelf.navigationController?.dismiss(animated: true, completion: nil)
         })
     }
     
